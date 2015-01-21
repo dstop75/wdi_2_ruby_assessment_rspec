39e11374bec172ee9a355e5cf00ec4735e6d94fa
Dan Stoppelman <dstop75@gmail.com>
2015-01-21 09:22:59 -0500

    added tests to math_spec.rb

--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -1,15 +1,34 @@
 require 'spec_helper'
 require_relative '../lib/math.rb'
 
-# Now you write some tests
-# These will look something like this:
-=begin
-
-RSpec.describe '#whatever method I'm testing' do
-  it 'should perform some kind of behavior' do
-    # I want 3 expectations with different parameters for each test
-    expect(method_call(parameters)).to eq something
+RSpec.describe '#square' do
+  it 'should return the value of x to the 2nd power' do
+    expect(square(1)).to eq 1
+    expect(square(-1)).to eq 1
+    expect(square(1.5)).to eq 2.25
   end
 end
 
-=end
+RSpec.describe '#cube' do
+  it 'should return the value of x to the 3rd power' do
+    expect(cube(0)).to eq 0
+    expect(cube(-2)).to eq -8
+    expect(cube(1.5)).to eq 3.375
+  end
+end
+
+RSpec.describe 'perimeter' do
+  it 'should return the perimeter of a square' do
+    expect(perimeter(1)).to eq 4
+    expect(perimeter(2)).to eq 8
+    expect(perimeter(1.5)).to eq 6
+  end
+end
+
+RSpec.describe 'cube_surface_area' do
+  it 'should return the surface area of a cube' do
+    expect(cube_surface_area(2)).to eq 24
+    expect(cube_surface_area(1)).to eq 6
+    expect(cube_surface_area(1.5)).to eq 13.5
+  end
+end
